68a00388a59b563013deb5592d11f75a0aa97999
Dallas <dallas.clarke622@gmail.com>
2021-03-08 13:47:02 -0800

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    items: [
+      {name: "Bananas", price: "$2.99", description: "Bundle of Bananas", img: "",},
+      {name: "Bread", price: "$3.99", description: "Loaf of Bread", img: "" },
+      {name: "Green Apples", price: "$4.49", decription: "Bag of Apples", img: ""},
+      {name: "Peanut Butter", price: "$3.50", decription: "Jar of Peanut Butter", img: ""},
+      {name: "Steak", price: "$15.96", decription: "14oz NY Strip", img: ""}
+    ],
+    searchQuery: ""
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>DUMMY TEXT</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
