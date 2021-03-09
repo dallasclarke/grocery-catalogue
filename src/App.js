0310bcb1595e3c35aa99d1c65efc78bdd905fbd7
Dallas <dallas.clarke622@gmail.com>
2021-03-09 14:09:19 -0800

    Add images for state and created folder structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 
+//Images of food for state.
+import bananas from "./img/bananas.jpeg";
+import bread from "./img/bread.jpeg";
+import apples from "./img/apples.jpeg";
+import peanutButter from "./img/peanutbutter.jpeg"
+import steak from "./img/steak.jpg";
+
 import "./App.css";
+
 
 class App extends Component {
   state = {
     items: [
-      {name: "Bananas", price: "$2.99", description: "Bundle of Bananas", img: "",},
-      {name: "Bread", price: "$3.99", description: "Loaf of Bread", img: "" },
-      {name: "Green Apples", price: "$4.49", decription: "Bag of Apples", img: ""},
-      {name: "Peanut Butter", price: "$3.50", decription: "Jar of Peanut Butter", img: ""},
-      {name: "Steak", price: "$15.96", decription: "14oz NY Strip", img: ""}
+      {name: "Bananas", price: "$2.99", description: "Bundle of Bananas", img: bananas,},
+      {name: "Bread", price: "$3.99", description: "Loaf of Bread", img: bread },
+      {name: "Green Apples", price: "$4.49", decription: "Bag of Apples", img: apples},
+      {name: "Peanut Butter", price: "$3.50", decription: "Jar of Peanut Butter", img: peanutButter},
+      {name: "Steak", price: "$15.96", decription: "14oz NY Strip", img: steak}
     ],
     searchQuery: ""
   }
@@ -19,6 +27,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>DUMMY TEXT</h1>
+        
       </div>
     )
   }
