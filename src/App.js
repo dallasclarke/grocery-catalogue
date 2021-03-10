ef1a44258a0f996768f1d5273ac593da8e5a8675
Dallas <dallas.clarke622@gmail.com>
2021-03-10 11:24:29 -0800

    More setting up with folders

--- src/App.js
+++ src/App.js
@@ -4,32 +4,63 @@ import React, { Component } from "react";
 import bananas from "./img/bananas.jpeg";
 import bread from "./img/bread.jpeg";
 import apples from "./img/apples.jpeg";
-import peanutButter from "./img/peanutbutter.jpeg"
+import peanutButter from "./img/peanutbutter.jpeg";
 import steak from "./img/steak.jpg";
 
 import "./App.css";
-
+import AddControls from "./components/Prem-Components/AddControls";
+import EditControls from "./components/Prem-Components/EditControls";
+import NavBar from "./components/Dallas-Components/Search/NavBar";
+import ItemDisplay from "./components/Dallas-Components/ItemDisplay/ItemDisplay";
 
 class App extends Component {
   state = {
     items: [
-      {name: "Bananas", price: "$2.99", description: "Bundle of Bananas", img: bananas,},
-      {name: "Bread", price: "$3.99", description: "Loaf of Bread", img: bread },
-      {name: "Green Apples", price: "$4.49", decription: "Bag of Apples", img: apples},
-      {name: "Peanut Butter", price: "$3.50", decription: "Jar of Peanut Butter", img: peanutButter},
-      {name: "Steak", price: "$15.96", decription: "14oz NY Strip", img: steak}
+      {
+        name: "Bananas",
+        price: "$2.99",
+        description: "Bundle of Bananas",
+        img: bananas,
+      },
+      {
+        name: "Bread",
+        price: "$3.99",
+        description: "Loaf of Bread",
+        img: bread,
+      },
+      {
+        name: "Green Apples",
+        price: "$4.49",
+        decription: "Bag of Apples",
+        img: apples,
+      },
+      {
+        name: "Peanut Butter",
+        price: "$3.50",
+        decription: "Jar of Peanut Butter",
+        img: peanutButter,
+      },
+      {
+        name: "Steak",
+        price: "$15.96",
+        decription: "14oz NY Strip",
+        img: steak,
+      },
     ],
-    searchQuery: ""
-  }
-
+    searchQuery: "",
+  };
 
   render() {
+    
     return (
       <div className="App">
         <h1>DUMMY TEXT</h1>
-        
+        <NavBar />
+        <ItemDisplay  /> 
+        <AddControls  />
+        <EditControls />
       </div>
-    )
+    );
   }
 }
 
---
+++ src/components/Dallas-Components/Search/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+
+function NavBar(props) {
+    return (
+        <div>
+            <h1>Test</h1>
+            <p></p>
+        </div>
+    )
+}
+
+export default NavBar;
