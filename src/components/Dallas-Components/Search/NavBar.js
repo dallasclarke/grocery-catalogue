e5ac559165ea383c003a5de30f03a6e8981ce1a2
Dallas <dallas.clarke622@gmail.com>
2021-03-11 16:34:42 -0800

    Finished styling nav. Did some work on the search bar to look better

--- src/components/Dallas-Components/Search/NavBar.js
+++ src/components/Dallas-Components/Search/NavBar.js
@@ -1,12 +1,22 @@
 import React from "react"
-
+import "./NavBar.css"
 
 function NavBar(props) {
     return (
-        <div>
-            <input type="text" placeholder="Search Items" onChange={props.searchInput} />
+      <div className="bar">
+          <h2>Catalogue</h2>
+        <div className="search">
+            <h3>Search</h3>
+          <div>
+            <input
+              type="text"
+              placeholder="Search..."
+              onChange={props.searchInput}
+            />
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default NavBar;
