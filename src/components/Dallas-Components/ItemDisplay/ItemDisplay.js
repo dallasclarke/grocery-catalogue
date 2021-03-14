42a4bdfb382a149de4cae4cd286f2623589f802e
Dallas <dallas.clarke622@gmail.com>
2021-03-14 16:52:55 -0700

    Added pagination to item display, just have styling left

--- src/components/Dallas-Components/ItemDisplay/ItemDisplay.js
+++ src/components/Dallas-Components/ItemDisplay/ItemDisplay.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./ItemDisplay.css";
 import ItemContext from "../../../context/ItemContext";
 import { EditControls } from "../../Prem-Components/EditControls";
 
 function ItemDisplay(props) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(3);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItem = props.items.slice(indexOfFirstItem, indexOfLastItem);
+
+  const displayItems = currentItem.map((item) => {
+    return (
+      <div className="items">
+        <li>{item.name}</li>
+        <img src={item.img} />
+        <li>{item.description}</li>
+        <EditControls data={item} />
+      </div>
+    );
+  });
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(props.items.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const handleClick = (e) => {
+    setCurrentPage(Number(e.target.id));
+  };
+
+  const renderPageNumber = pageNumbers.map((number) => {
+    return (
+      <li key={number} id={number} onClick={handleClick}>
+        {number}
+      </li>
+    );
+  });
+
   return (
     <div>
       <div className="item-display">
-        <ul>
-          {props.items.map((item) => (
-            <div className="items">
-              <li>{item.name}</li>
-              <img src={item.img} />
-              <li>{item.price}</li>
-              <li>{item.description}</li>
-              {/* changes by p */}
-              <EditControls data={item}/>
-            </div>
-          ))}
-        </ul>
+        <ul>{displayItems}</ul>
+        <ul>{renderPageNumber}</ul>
       </div>
-      )}
     </div>
   );
 }
